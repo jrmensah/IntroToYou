dfab54fc0e9ba4099e6a0724b6613bf984917b16
jrmensah <jmensah4@student.umuc.edu>
2017-10-02 16:40:57 -0400

    First commit- Getting and Storing User Input

---
+++ src/UserInput.java
@@ -0,0 +1,32 @@
+import java.util.Scanner;
+
+public class UserInput {
+    public static void main (String args[])
+    {
+        String name, level, major, location;
+
+        System.out.println("Hello, my name is");
+        Scanner keyboard = new Scanner(System.in);
+        name = keyboard.nextLine();
+        System.out.println("Hello, my name is:" +name);
+
+        System.out.println("My level of experience with Java:");
+        level = keyboard.nextLine();
+        keyboard.nextLine();
+
+        System.out.println("My level of experience with Java:" +level);
+
+        System.out.println("Major in College");
+        major = keyboard.nextLine();
+        keyboard.nextLine();
+
+        System.out.println("Major in College:" +major);
+
+        System.out.println("Where are you from?");
+        location = keyboard.nextLine();
+        keyboard.nextLine();
+
+        System.out.println("Where are you from?:" +location);
+
+    }
+}
